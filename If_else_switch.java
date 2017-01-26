26a50bbd0927b56b54b91645341ae73f6ad63ffc
buruayan <bryan.kahiga@strathmore.edu>
2017-01-26 19:25:00 +0300

    Add files via upload

---
+++ If_else_switch.java
@@ -0,0 +1,26 @@
+
+package if_else_switch;
+public class If_else_switch{
+    public static void main(String []args){
+        int x=30;
+        switch (x){
+            case 1:
+                System.out.println("Value of x is 10");
+                break;
+                case 2:
+                System.out.println("Value of x is 20");
+                break;
+                    case 3:
+                System.out.println("Value of x is 30");
+                break;
+                         default:
+                System.out.println("invalid");
+        }
+        System.out.println("The value of x is " +x);
+    }
+}
+        
+/**
+ *
+ * @author 74679
+ */
