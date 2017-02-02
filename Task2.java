1d51daff4b02bcd06924fd3970f3cf201b2e8135
buruayan <bryan.kahiga@strathmore.edu>
2017-02-02 19:10:15 +0300

    Add files via upload

--- Task2.java
+++ Task2.java
@@ -1,32 +1,50 @@
 
 package task2;
-import java.util.Scanner;
-public class Task2 { 
+import java.util.Scanner; // needed for Scanner
+/* A Java program that demonstrates console based input and output. */
+public class Task2 {
 
+ // Create a single shared Scanner for keyboard input
+ private static final Scanner scanner = new Scanner( System.in );
  // Program execution starts here
- public static void main(String [] args )
+ public static void main ( String [] args )
  {
-     Scanner scanner=new Scanner(System.in);
-    
  // Prompt the user1
- System.out.print( "Why are you backing that team to win?: " );
+ System.out.print( "Do you reckon that the team you back will win?: " );
  // Read a line of text from the user.
- String a = scanner.nextLine();
- // Prompt the user2
+ String input1 = scanner.nextLine();
+  // Prompt the user2
   System.out.print( "What has been their recent form?: " );
-  String b = scanner.nextLine();
-  // Prompt the user3
+  String input2 = scanner.nextLine();
+   // Prompt the user3
  System.out.print( "Are there any injured players?: " );
- String c = scanner.nextLine();
+ String input3 = scanner.nextLine();
    // Prompt the user4
  System.out.print( "How do you expect the manager to line up his squad?: " );
   int squad = scanner.nextInt();
-  // Prompt the user3
- System.out.print( "What is your final score prediction?: " );
- int finale = scanner.nextInt();
-  // Prompt the user3
- System.out.print( "Lastly,are the bookies odds in your favour?: " );
+  // Prompt the user5
+  System.out.print( "What are the betting odds in favour of your team?: " );
  float d = scanner.nextFloat();
+ System.out.print( "How much money will you gamble on a win?: " );
+ int stake = scanner.nextInt();
+ float winnings=stake*d;
+ System.out.print( "How many points did the team collect?: " );
+  int ans = scanner.nextInt();
+  if(ans==3){
+ System.out.println("Congratulations you have won!!"+winnings +"sh");
+}
+  else if(ans<3){
  
+ System.out.println("Loss,Please play responsibly");
+
  }
 }
+}
+
+
+
+
+/**
+ *
+ * @author 74679
+ */
